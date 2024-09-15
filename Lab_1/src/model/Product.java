88bccaf7320032085f8c63697535ba4dddbbfea3
unknown <raut.ni@northeastern.edu>
2024-09-15 19:38:19 -0400

    Swing UI

---
+++ Lab_1/src/model/Product.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Wafflecrag
+ */
+public class Product {
+    String name;
+    String description;
+    String availablity;
+    String price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAvailablity() {
+        return availablity;
+    }
+
+    public void setAvailablity(String availablity) {
+        this.availablity = availablity;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+    
+}
