61d94753fd29c41af3778cbfca4a023c5e000f52
unknown <raut.ni@northeastern.edu>
2024-11-10 14:28:00 -0500

    Lab6 skeleton

---
+++ Lab_6/src/model/Order.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Wafflecrag
+ */
+public class Order {
+    
+    ArrayList<OrderItem> orderItemList;
+
+    public Order() {
+        this.orderItemList = new ArrayList<OrderItem>();
+    }
+
+    public ArrayList<OrderItem> getOrderItemList() {
+        return orderItemList;
+    }
+
+    public void setOrderItemList(ArrayList<OrderItem> orderItemList) {
+        this.orderItemList = orderItemList;
+    }
+    
+    public void addNewOrder
+}
