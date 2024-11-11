9ec26200654ef1815f2aa4733c6d8d274138a4d4
unknown <raut.ni@northeastern.edu>
2024-11-10 20:51:37 -0500

    CheckOut

--- Lab_6/src/ui/CustomerRole/BrowseProductsJPanel.java
+++ Lab_6/src/ui/CustomerRole/BrowseProductsJPanel.java
@@ -337,6 +337,22 @@ public class BrowseProductsJPanel extends javax.swing.JPanel {
 
     private void btnCheckOutActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCheckOutActionPerformed
         // TODO add your handling code here:
+        
+        masterOrderList.addNewOrder(currentOrder);
+        currentOrder = new Order();
+        
+        populateCombo();
+        populateProductTable();
+        populateCartTable();
+        
+        txtNewQuantity.setText("");
+        txtSalesPrice.setText("");
+        txtSearch.setText("");
+        
+        spnQuantity.setValue(0);
+        
+        JOptionPane.showMessageDialog(this, "Thank you for your purchase.");
+
 
     }//GEN-LAST:event_btnCheckOutActionPerformed
 
@@ -369,6 +385,9 @@ public class BrowseProductsJPanel extends javax.swing.JPanel {
             
             item.getProduct().setAvail(item.getProduct().getAvail()+oldQuant-quant);
             item.setQuantity(quant);
+            
+            populateCartTable();
+            populateProductTable();
 
     }//GEN-LAST:event_btnModifyQuantityActionPerformed
 
@@ -382,10 +401,37 @@ public class BrowseProductsJPanel extends javax.swing.JPanel {
 
     private void btnRemoveOrderItemActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRemoveOrderItemActionPerformed
 
+        int selectedRowIndex = tblCart.getSelectedRow();
+        if (selectedRowIndex < 0) {
+            JOptionPane.showMessageDialog(this, "Please select the order item first");
+            return;
+        }
+        
+        OrderItem item = (OrderItem) tblCart.getValueAt(selectedRowIndex, 0);
+        int quant = 0;
+        
+        item.getProduct().setAvail(item.getProduct().getAvail()+ item.getQuantity());
+        currentOrder.deleteItem(item);
+        
+        populateCartTable();
+        populateProductTable();
+        
     }//GEN-LAST:event_btnRemoveOrderItemActionPerformed
 
     private void btnViewOrderItemActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnViewOrderItemActionPerformed
 
+        int selectedRowIndex = tblCart.getSelectedRow();
+        if (selectedRowIndex < 0) {
+            JOptionPane.showMessageDialog(this, "Please select the item first");
+            return;
+        }
+        
+        OrderItem item = (OrderItem) tblCart.getValueAt(selectedRowIndex, 0);
+        ViewOrderItemDetailJPanel voidp = new ViewOrderItemDetailJPanel(userProcessContainer, item);
+        userProcessContainer.add("ViewOrderItemDetailJPanel", voidp);
+        CardLayout layout = (CardLayout) userProcessContainer.getLayout();
+        layout.next(userProcessContainer);
+        
     }//GEN-LAST:event_btnViewOrderItemActionPerformed
 
     private void btnAddToCartActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAddToCartActionPerformed
