ece8b658095439b9b644fc48741fc21c999be9f9
unknown <raut.ni@northeastern.edu>
2024-11-24 15:04:37 -0500

    q1

---
+++ Lab_8/src/analytics/UserMapComparator.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package analytics;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import model.User;
+/**
+ *
+ * @author Wafflecrag
+ */
+public class UserMapComparator {
+    
+}
